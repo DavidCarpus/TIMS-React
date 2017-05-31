8b041d56c5bdac8c331544500319c4909ad289f7
David Carpus <David.Carpus@gmail.com>
2017-05-31 11:17:11 -0400

    Add mode (dev,prod,test) to 'config' to allow code to branch based on env

--- nodeapi/config.js
+++ nodeapi/config.js
@@ -4,14 +4,17 @@ var test_configuration = require('./private/configuration_test.json');
 
 let mode=process.env.NODE_ENV||'development'
 module.exports = function(){
-    console.log('Mode:' + mode);
+    // console.log('Mode:' + mode);
     switch(mode){
         case 'development':
-            return dev_configuration;
+            // return dev_configuration;
+            return Object.assign({}, dev_configuration, {mode: mode});
         case 'production':
-            return prod_configuration;
+        // return prod_configuration;
+            return Object.assign({}, prod_configuration, {mode: mode});
         case 'test':
-            return test_configuration;
+        // return test_configuration;
+            return Object.assign({}, test_configuration, {mode: mode});
         // default:
         //     return dev_configuration;
     }
