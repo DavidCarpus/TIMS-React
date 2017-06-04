4aa162acb2634d90bc453350cf4a16aac589dbc6
David Carpus <David.Carpus@gmail.com>
2017-06-04 16:09:23 -0400

    Stop verbose db connection lost error messages

--- nodeapi/routes/index.js
+++ nodeapi/routes/index.js
@@ -22,7 +22,7 @@ function handleDisconnect() {
   });                                     // process asynchronous requests in the meantime.
                                           // If you're also serving http, display a 503 error.
   connection.on('error', function(err) {
-    console.log('db error', JSON.stringify(err));
+    // console.log('db error', JSON.stringify(err));
     if(err.code === 'PROTOCOL_CONNECTION_LOST') { // Connection to the MySQL server is usually
       handleDisconnect();                         // lost due to either server restart, or a
     } else {                                      // connnection idle timeout (the wait_timeout
