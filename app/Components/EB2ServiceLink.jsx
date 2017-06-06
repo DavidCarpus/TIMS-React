249d2857fb4102d0d948cbc7c577d8bbac2570e2
David Carpus <David.Carpus@gmail.com>
2017-06-06 14:06:51 -0400

    Data structure field name changed

--- app/Components/EB2ServiceLink.jsx
+++ app/Components/EB2ServiceLink.jsx
@@ -5,7 +5,7 @@ export default class EB2ServiceLink extends React.Component {
         return 'http://www.eb2gov.com/Images/Themes/' + service.img
     }
     serviceLink(service){
-        return 'https://www.eb2gov.com/scripts/eb2gov.dll/' + service.lnk
+        return 'https://www.eb2gov.com/scripts/eb2gov.dll/' + service.urlLink
     }
     render() {
         var service=this.props.service
