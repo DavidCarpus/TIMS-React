b8d2275e97636321f0f37b5b650716885f67f84a
David Carpus <David.Carpus@gmail.com>
2017-07-15 07:21:48 -0400

    Moved helpful links import from migrations to seeds

--- server/libs/db/migrations/20170617080708_helpful-links.js
+++
@@ -1,9 +0,0 @@
-
-exports.up = function(knex, Promise) {
-    console.log('Adding table for helpful links')
-    return knex('ListData').insert({listName:'HelpfulLinks', datadesc:'Test link desc', fileLink:'http://www.google.com' })
-}
-
-exports.down = function(knex, Promise) {
-    return knex('ListData').where({listName:'HelpfulLinks', datadesc:'Test link desc', fileLink:'http://www.google.com' }).del()
-};
