75085cdd41c20e5fa145e46a043740c1f2f68eed
David Carpus <David.Carpus@gmail.com>
2017-08-02 15:17:04 -0400

    Move municipal ‘name’ to configuration

--- src/Client/pages/About/About.jsx
+++ src/Client/pages/About/About.jsx
@@ -19,7 +19,7 @@ render() {
     return (
         <div>
             <Col md={10}  mdPush={1} id="contentArea"  >
-            <h1 style={{textAlign:'center'}}>About the Town of Milton <br/>New Hampshire</h1>
+            <h1 style={{textAlign:'center'}}>About the {this.props.Config.municipalLongName}</h1>
                 <RawText groupPageText={groupPageText} block='description' />
             </Col>
             <Col md={1} mdPull={10}><Aside group={group}   /></Col>
