3c7eb36a2a18cef22113dfdbe2960e4b731175e4
David Carpus <David.Carpus@gmail.com>
2017-08-07 10:02:45 -0400

    Forgot the server/api when moving common configuration data to its own file and merge them as needed.

--- server/config.js
+++ server/config.js
@@ -1,23 +1,65 @@
+var base_configuration = require('./private/configuration.json');
 var dev_configuration = require('./private/configuration_dev.json');
 var prod_configuration = require('./private/configuration_prod.json');
 var test_configuration = require('./private/configuration_test.json');
 
+//======================================
+//======================================
+// https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge
+//======================================
+/**
+ * Simple object check.
+ * @param item
+ * @returns {boolean}
+ */
+function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
+
+//======================================
+/**
+ * Deep merge two objects.
+ * @param target
+ * @param ...sources
+ */
+ function mergeDeep(target, ...sources) {
+  if (!sources.length) return target;
+  const source = sources.shift();
+
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
+
+  return mergeDeep(target, ...sources);
+}
+//======================================
+//======================================
+
+//======================================
 let mode=process.env.NODE_ENV||'development'
+// mode='test'
+
 module.exports = function(){
-    // console.log('Mode:' + mode);
+    let mergedConf = mergeDeep({}, base_configuration);
+    mergedConf = mergeDeep(mergedConf,
+        {mode: mode, ATTACHMENT_DIR:__dirname +  mergedConf.attachmentPath }
+    );
+
     switch(mode){
         case 'development':
-            // return dev_configuration;
-            return Object.assign({}, dev_configuration,
-                {mode: mode, ATTACHMENT_DIR:__dirname +dev_configuration.attachmentPath });
+            // console.log(mergeDeep(mergedConf, dev_configuration ));
+            return mergeDeep(mergedConf, dev_configuration );
         case 'production':
-        // return prod_configuration;
-            return Object.assign({}, prod_configuration,
-                {mode: mode, ATTACHMENT_DIR:__dirname +  prod_configuration.attachmentPath });
+            return mergeDeep(mergedConf, prod_configuration );
         case 'test':
-        // return test_configuration;
-            return Object.assign({}, test_configuration,
-                {mode: mode, ATTACHMENT_DIR:__dirname +  test_configuration.attachmentPath });
+            return mergeDeep(mergedConf, test_configuration );
         // default:
         //     return dev_configuration;
     }
