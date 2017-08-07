4e3e18ff0786637300f2aaf16134cd3470f757e7
David Carpus <David.Carpus@gmail.com>
2017-08-07 09:58:13 -0400

    Move common configuration data to its own file and merge them as needed.

--- src/Client/config.js
+++ src/Client/config.js
@@ -1,23 +1,70 @@
 var dev_configuration = require('privateConfig/configuration_dev.json');
 var prod_configuration = require('privateConfig/configuration_prod.json');
 var test_configuration = require('privateConfig/configuration_test.json');
+var base_configuration = require('privateConfig/configuration.json');
+//======================================
+//======================================
+// https://stackoverflow.com/questions/27936772/how-to-deep-merge-instead-of-shallow-merge
+//======================================
+/**
+ * Simple object check.
+ * @param item
+ * @returns {boolean}
+ */
+function isObject(item) {
+  return (item && typeof item === 'object' && !Array.isArray(item));
+}
 
+//======================================
+/**
+ * Deep merge two objects.
+ * @param target
+ * @param ...sources
+ */
+ function mergeDeep(target, ...sources) {
+  if (!sources.length) return target;
+  const source = sources.shift();
 
-let currEnv = process.env.NODE_ENV;
+  if (isObject(target) && isObject(source)) {
+    for (const key in source) {
+      if (isObject(source[key])) {
+        if (!target[key]) Object.assign(target, { [key]: {} });
+        mergeDeep(target[key], source[key]);
+      } else {
+        Object.assign(target, { [key]: source[key] });
+      }
+    }
+  }
 
-// if (location.href.indexOf('localhost') > 0) {      currEnv = 'development' }
- if (location.href.indexOf('test.miltonnh') > 0) {      currEnv = 'test' } // eslint-disable-line no-restricted-globals
+  return mergeDeep(target, ...sources);
+}
+//======================================
+//======================================
 
+//======================================
+let mode=process.env.BUILD_MODE || process.env.NODE_ENV || 'production'
+// mode='test'
 
 module.exports = function(){
-    switch(currEnv){
+    let mergedConf = mergeDeep({}, base_configuration);
+    mergedConf = mergeDeep(mergedConf,
+        {mode: mode, ATTACHMENT_DIR:__dirname +  mergedConf.attachmentPath }
+    );
+
+    switch(mode){
         case 'development':
-            return dev_configuration;
+            mergedConf = mergeDeep(mergedConf, dev_configuration );
+            break;
         case 'production':
-            return prod_configuration;
+            mergedConf = mergeDeep(mergedConf, prod_configuration );
+            break;
         case 'test':
-            return test_configuration;
+            mergedConf = mergeDeep(mergedConf, test_configuration );
+            break;
         default:
-            return dev_configuration;
+            return mergedConf;
     }
+    // console.log('process.env.BUILD_MODE:', process.env.BUILD_MODE);
+    // console.log('mergedConf:',mergedConf );
+    return mergedConf;
 };
