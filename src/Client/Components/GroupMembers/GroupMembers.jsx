14cb6ca74ebd227d3df8ef5ea746598729b28d0e
David Carpus <David.Carpus@gmail.com>
2017-09-06 10:26:23 -0400

    Add section to group members to display the group address and hours

--- src/Client/Components/GroupMembers/GroupMembers.jsx
+++ src/Client/Components/GroupMembers/GroupMembers.jsx
@@ -9,20 +9,56 @@ function memberBlock(member, index, cols){
         </div>
     )
 }
+function addressBlock(addressstr) {
+    if (typeof addressstr === 'undefined' || addressstr.length === 0) {        return(null);    }
+    let addressElements =  addressstr.split('\n');
+    return (
+        <div id='addressBlock'>
+            <h3>Address</h3>
+            <address>
+            {addressElements.map( (address, addressLine) =>
+                <div key={addressLine}>{address}</div>
+            )}
+        </address>
+    </div>     )
+}
+function hourBlock(hourstr) {
+    if (typeof hourstr === 'undefined' || hourstr.length === 0) {        return(null);    }
+    let hours =  hourstr.split('\n');
+    return (
+        <div id='hours'>
+            <br/>
+            <h3>Hours</h3>
+            {hours.map( (line, index) =>
+                <div key={index}>{line}</div>
+            )}
+         </div>
+    )
 
-export default function GroupMembers({groupName, title, members, loading}){
+}
+
+
+export default function GroupMembers({groupName, title, members, loading, addressstr, hourstr, groupEmail, showTerm=true, showEmail}){
     if ( loading) {         return (<div>Loading</div>)     }
     if (members.length === 0) {        return(null);    }
 
-     var id = groupName + '_Members'
+     let cols=['Office', 'Name', 'Phone'];
+     if(showTerm) cols.push('Term')
+     if(showEmail) cols.push('Email')
 
-     let cols=['Office', 'Term', 'Name', 'Phone'];
      return (
-         <div id={id} className='groupMembers'>
+         <div id='groupMembers'>
+             <a id="groupMembers-bookmark">groupMembers Start</a>
              <h2>{title}</h2>
-                 {  cols.map((col, colNum) => <div key={colNum} className={col.toLowerCase()}>{col}</div>) }
+                 {  cols.map((col, colNum) =>
+                     <div key={colNum} className={col.toLowerCase()}>{col}</div>)
+                 }
                  {members.map( (member, index) =>
                      memberBlock(member, index, cols) )}
+
+                 {groupEmail ? groupEmail: ''}
+                 {hourBlock (hourstr)}
+                 {addressBlock (addressstr)}
          </div>
      )
  }
--- src/Client/Components/GroupMembers/GroupMembersC.jsx
+++ src/Client/Components/GroupMembers/GroupMembersC.jsx
@@ -3,11 +3,18 @@ import GroupMembersUI from './GroupMembers'
  import { connect } from 'react-redux'
 
 const mapStateToProps = (state, ownProps) => {
-    var members = ownProps.group.members || []
-    // console.log('GroupMembersUI:state:', state);
+    const  members = ownProps.group.members || []
+    const showTerm= typeof ownProps.showTerm === 'undefined' && true
+    // console.log('GroupMembersUI:group:', ownProps.group);
+    // console.log('GroupMembersUI:', ownProps);
   return {
       members: members,
-      loading: state.OrganizationalUnits.loading
+      loading: state.OrganizationalUnits.loading,
+      showTerm: showTerm,
+      hourstr: ownProps.group.hours || '',
+      addressstr: ownProps.group.address || '',
+    //   groupAddress: ['PO Box 310', 'Milton, NH 03851']
+        // groupEmail: 'assessing@miltonnh-us.com'
   };
 }
 
