4aff675bac3a4b1115426dbe7f7a968f3e1cd843
David Carpus <David.Carpus@gmail.com>
2017-09-18 06:56:56 -0400

    Add date posted and group to display of notices.

--- src/Client/Components/NoticesList/NoticesList.jsx
+++ src/Client/Components/NoticesList/NoticesList.jsx
@@ -2,11 +2,13 @@ import React from 'react';
 import  './Notices.css'
 import SmartLink from '../SmartLink'
 
+const dateStr =(date)=> date && (date.getUTCMonth()+1) + '/' + date.getUTCDate()+ '/' + date.getUTCFullYear()
+const maxDisplay = 5
 export default function  NoticesList({alertnotices, notices}){
     if (notices.length === 0) {        return(null);    }
 
     return (
-        <section id='notices'>
+        <section id='notices' >
             <a id="Notices-bookmark">Notices Start</a>
             <div id='alerts'>
                 <ul className="hidebullets">
@@ -20,12 +22,23 @@ export default function  NoticesList({alertnotices, notices}){
 
             <h2>Notices</h2>
             <ul className="hidebullets">
-                {notices.map( (notice,index) =>
+                {notices.slice(0,maxDisplay).map( (notice,index) =>
                     <li key={notice.id || index}>
-                        <SmartLink link={notice.link || ""} id={notice.id} linkText={notice.description} />
+                        <SmartLink link={notice.link || ""} id={notice.id} linkText={notice.description || (notice.pageLink + "-"+ notice.recordtype)} />
+                        <span className="posted">
+                            (Posted {dateStr(new Date(notice.date))})
+                            {typeof notice.pageLink !== 'undefined' && notice.pageLink !== 'UNK'  ?
+                                "- " + notice.pageLink :
+                                ""
+                            }
+                        </span>
                     </li>
                 )}
             </ul>
+            {notices.length > maxDisplay &&
+                <SmartLink link="/PublicRecords/Notices" id="0" linkText=" ... More" />
+            }
+
         </section>
     )
 }
