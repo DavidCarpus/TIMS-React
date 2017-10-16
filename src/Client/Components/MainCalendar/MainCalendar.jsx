c1e791978824901341f19117f611ec943ad1529e
David Carpus <David.Carpus@gmail.com>
2017-10-16 14:41:34 -0400

    Seperate view and control for calendar better. Push more to control.

--- src/Client/Components/MainCalendar/MainCalendar.jsx
+++ src/Client/Components/MainCalendar/MainCalendar.jsx
@@ -2,16 +2,6 @@ import React from 'react';
 import  './MainCalendar.css'
 import {Link} from 'react-router-dom';
 
-import  startOfMonth from 'date-fns/start_of_month'
-import  startOfWeek from 'date-fns/start_of_week'
-import  endOfMonth from 'date-fns/end_of_month'
-import  endOfWeek from 'date-fns/end_of_week'
-// import addMonths from 'date-fns/add_months'
-import addDays from 'date-fns/add_days'
-import getISOWeek from 'date-fns/get_iso_week'
-import isSameDay from 'date-fns/is_same_day'
-// import isThisMonth from 'date-fns/is_this_month'
-// import getMonth from 'date-fns/get_month'
 
 import { Col, Row } from 'reactstrap';
 import SmartLink from '../SmartLink'
@@ -25,128 +15,44 @@ var monthNames = [
   "November", "December"
 ];
 
-function outputCalDate(date, events, pivotDate) {
-    let chkDate = date
-    // console.log('chkDate:', chkDate);
 
-    let matchDates = events.filter(evt => isSameDay(new Date(evt.startDate), chkDate ))
-
-    // If date has an event set className to eventDay
-    let className= matchDates.length > 0? 'eventDay':'calDay'
-    if (date.getMonth() !== pivotDate.getMonth()) {
-        // console.log('outsideMonth' , pivotDate.getMonth());
-        className += ' outsideMonth'
-    }
-    return (
-        <span key={date.getMonth() + '-' + date.getDate()} className={className}>{date.getDate()}</span>
-    )
-}
-
-function week(dates, events, pivotDate) {
-    return (
-    <div  key={getISOWeek(addDays(dates[0], 1))} >
-        {dates.map(date =>
-            outputCalDate(date, events, pivotDate)
-        )}
-        <br/>
-    </div>
-    )
-}
-
-function currentMonthBlock(events) {
-    // let currDate  = new Date();
-    let currDate  = events[0].startDate
-    // console.log(events[0]);
-    // let matchDates = events.filter(evt => isThisMonth(new Date(evt.startDate) ))
-    // // console.log('matchDates:', currDate, matchDates);
-    // if (matchDates.length === 0) {
-    //     // console.log('No events remaining this month. Use Next Month.');
-    //     currDate  = addMonths(currDate, 1)
-    // }
-
-    const startDate = startOfWeek(startOfMonth(currDate) );
-    const endDate = endOfWeek(endOfMonth(currDate) );
-
-    let date = startDate;
-    // let days = []
-    let weeks = {}
-    while (date < endDate) {
-        let weekOfMonth = getISOWeek(addDays(date, 1)) // We want Sunday as start of week
-        if (! Array.isArray(weeks[weekOfMonth])) {
-            weeks[weekOfMonth]=[]
-        }
-        weeks[weekOfMonth].push(date)
-        // days.push(date)
-        date = addDays(date, 1)
-    }
+function currentMonthBlock(calDataByWeek) {
+    const calDateStyle = (dayData) => (dayData.events.length > 0? 'eventDay':'calDay') +
+        (getSundayOfWeekNumber(1).getMonth() !== dayData.date.getMonth() ?   ' outsideMonth' : '')
+    const getSundayOfWeekNumber = (week) => calDataByWeek[Object.keys(calDataByWeek)[week]][0].date
+    const getMonthName= () => monthNames[getSundayOfWeekNumber(1).getMonth()]
 
     return (
         <div id='currentMonthBlock'>
-            <div className='MonthTitle'>{monthNames[currDate.getMonth()]}</div>
-            {Object.keys(weeks).map(weekNum =>
-                    week(weeks[weekNum], events, currDate )
+            <div className='MonthTitle'>{getMonthName()}</div>
+            {calDataByWeek.map( (outputWeek, weekNum) =>
+                <div key={'week'+weekNum}>
+                {outputWeek.map(day =>
+                    <span key={(day.date.getMonth()+1) + '-' + day.date.getDate()} className={calDateStyle(day)}>{day.date.getDate()}</span>
+                )}
+                </div>
             )}
             <br/>
         </div>
     )
-/*
-{'weeks ' + JSON.stringify(weeks)}  <br/>
-{'events ' + JSON.stringify(events)}  <br/>
-
-*/
-
 }
-const AgendaLink = (agendaID) => {
-    const link = configuration.ui.ROOT_URL + 'fetchfile/' + agendaID.agendaID;
-    const lnkStyle='agendaIcon'
-    const linkText='Agenda'
 
-    return (
+
+const AgendaLink = (agendaID) =>
         <Link  className='agendaIcon'
             to={configuration.ui.ROOT_URL + 'fetchfile/' + agendaID.agendaID}
             onClick={(event) => {
-                event.preventDefault(); window.open(link);
+                event.preventDefault(); window.open(configuration.ui.ROOT_URL + 'fetchfile/' + agendaID.agendaID);
             }}>
         <img  src='/images/icons/Agenda.png' className='agendaIcon'   alt="Agenda" title="Agenda" />
         </Link >
-    )
 
-}
-// return (<Link to={link} className={lnkStyle} target="_blank" onClick={(event) => {
-//     event.preventDefault(); window.open(link);
-// }} >{linkText}</Link>)
-
-
-
-function eventList(rawCalData) {
-    // console.log("MainCalendar:calendarData:" , rawCalData);
-    const timeStamp = (date) => (date.getHours()+date.getMinutes()+date.getSeconds() > 0) ?
-        (date.getHours() > 12 ? ""+(date.getHours()-12): (date.getHours()))  + ":" +
-        (date.getMinutes() < 10 ? "0" + date.getMinutes(): date.getMinutes())  +
-        (date.getHours() > 12 ? ' PM': ' AM'):
-        ""
-
-    let calendarData = rawCalData.map( entry => {
-        let sdate =  new Date(entry.startDate)
-
-        return {
-            dateStr: monthNames[sdate.getMonth()] + ' ' + sdate.getDate(),
-            sdate: sdate,
-            time: timeStamp(sdate) + (timeStamp(sdate).length > 0?'-':"") + timeStamp(new Date(entry.endDate)),
-            orig: entry,
-            id:entry.id,
-            agendaID: entry.publicRecords ? entry.publicRecords.id: null,
-            description:entry.summary
-        }
-    }).sort((a,b) =>  a.sdate -b.sdate )
-    console.log("eventList:calendarData:" , calendarData);
-    // <div className="date">{entry.sdate.getMonth() + '-' + entry.sdate.getDate()}</div>
-    // {entry.agendaID && <div className="date">Agenda</div>}
+function eventList(evntLst) {
     return (
-<div id="eventList">{calendarData.map( (entry) =>
+<div id="eventList">{evntLst.map( (entry) =>
         <div className="entry" key={entry.id} >
             <div className="date">{entry.dateStr} {entry.agendaID && <AgendaLink agendaID={entry.agendaID}></AgendaLink>}</div>
-                <div className="date">{entry.time}</div>
+                <div className="date">{entry.timestamp}</div>
             <div className="description">{entry.description}</div>
         </div>
     )}
@@ -162,11 +68,8 @@ export default class MainCalendar extends React.Component {
 
     render() {
     if ( this.props.loading) {         return (<div>Loading</div>)     }
-    if (this.props.calendarData.length === 0) {        return(null);    }
-
-    const sortedData = this.props.calendarData.sort((a,b) =>  a.startDate -b.startDate )
-    console.log('this.props.calendarData', sortedData);
-    // this.props.fetchCalendarDocuments(sortedData[0].startDate, sortedData[sortedData.length-1].startDate );
+    if (Object.keys(this.props.calDataByWeek).length   === 0 ) { return(null);  }
+    if (Object.keys(this.props.eventList).length   === 0 ) { return(null);  }
 
         return (
             <section id='MainCalendar' className="MainCalendar">
@@ -174,22 +77,13 @@ export default class MainCalendar extends React.Component {
                 <h2>Upcoming Events</h2>
                     <Row>
                         <Col  md='5'  xs={{size:12}} id='calBlock'>
-                        {currentMonthBlock(this.props.calendarData)}
+                            {currentMonthBlock(this.props.calDataByWeek)}
                         </Col>
                         <Col  md='7'  xs={{size:12}} id='listBlock'>
-                        {eventList(this.props.calendarData)}
+                            {eventList(this.props.eventList)}
                     </Col>
                     </Row>
         </section>
         );
     }
 }
-
-// {calendarData.map( (entry) =>
-//     <div className="entry" key={entry.id} >
-//         <div className="date">{entry.date}</div>
-//         <div className="description">{entry.description}</div>
-//     </div>
-// )}
-        // console.log("NON recurringEvents in range:", calendarData.filter(isNotRecurringEvent).filter(eventInRange));
-        // console.log("RecurringEvents in range:", calendarData.filter(isRecurringEvent).filter(eventInRange));
--- src/Client/Components/MainCalendar/MainCalendarC.jsx
+++ src/Client/Components/MainCalendar/MainCalendarC.jsx
@@ -2,6 +2,43 @@
 import MainCalendar from './MainCalendar'
  import { connect } from 'react-redux'
 import { fetchCalendarData } from '../../actions/CalendarData'
+import  startOfMonth from 'date-fns/start_of_month'
+import  startOfWeek from 'date-fns/start_of_week'
+import  endOfMonth from 'date-fns/end_of_month'
+import  endOfWeek from 'date-fns/end_of_week'
+import addDays from 'date-fns/add_days'
+import getISOWeek from 'date-fns/get_iso_week'
+import isSameDay from 'date-fns/is_same_day'
+
+// var monthNames = [
+//   "January", "February", "March",
+//   "April", "May", "June", "July",
+//   "August", "September", "October",
+//   "November", "December"
+// ];
+
+var monthAbbreviations = [
+  "Jan", "Feb", "Mar",
+  "Apr", "May", "Jun", "Jul",
+  "Aug", "Sep", "Oct",
+  "Nov", "Dec"
+];
+
+const dateArray = (start , end) =>{
+    let date = start;
+    let results = []
+    while (date < end) {
+        results.push(date)
+        date = addDays(date, 1)
+    }
+    return results
+}
+const timeStampFromDate = (date) => (date.getHours()+date.getMinutes()+date.getSeconds() > 0) ?
+    (date.getHours() > 12 ? ""+(date.getHours()-12): (date.getHours()))  + ":" +
+    (date.getMinutes() < 10 ? "0" + date.getMinutes(): date.getMinutes())  +
+    (date.getHours() > 12 ? ' PM': ' AM'):
+    ""
+
 //-----------------------------------------------------------------------------
 //-----------------------------------------------------------------------------
 const mapStateToProps = (state, ownProps) => {
@@ -14,8 +51,34 @@ const mapStateToProps = (state, ownProps) => {
        return evt;
     })
 
+    const eventMatchesDate = (date, evt) => isSameDay(new Date(evt.startDate), date )
+    let calDataByWeek = calendarData.length <=0 ? {}: dateArray(startOfWeek(startOfMonth(calendarData[0].startDate) ), endOfWeek(endOfMonth(calendarData[0].startDate) ))
+        .map(date => (
+            {week: getISOWeek(addDays(date,1)), date:date, events: calendarData.filter( event => eventMatchesDate(date, event) ) })
+        )
+        .reduce( (acc, value) => {
+            if ((typeof acc[value.week] === 'undefined')) { acc[value.week] = [] }
+            acc[value.week].push(value)
+            return acc
+        }, [])
+
+    let eventList = calendarData.length <=0 ? {}: calendarData
+        .sort( (a,b)=> a.startDate - b.startDate)
+        .map(evt => {
+            // const evtObj = {
+            return {
+                dateStr: monthAbbreviations[evt.startDate.getMonth()] + ' ' + evt.startDate.getDate(),
+                timestamp: timeStampFromDate(evt.startDate) + (timeStampFromDate(evt.startDate).length > 0?'-':"") + timeStampFromDate(new Date(evt.endDate)),
+                id:evt.id,
+                agendaID: evt.publicRecords ? evt.publicRecords.id: null,
+                description:evt.summary
+            }
+            // console.log('evt', evtObj);
+        })
+
     return {
-        calendarData: calendarData,
+        calDataByWeek:calDataByWeek,
+        eventList:eventList,
         group: ownProps.group,
     };
 }
